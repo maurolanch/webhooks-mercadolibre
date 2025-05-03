5d43c94277d23403c54b931b0176e7d51f90d3c9
Mauricio L <maurolanm@gmail.com>
2025-05-03 09:47:41 -0500

    Mostrando webhooks en el log

--- main.py
+++ main.py
@@ -2,18 +2,26 @@ from flask import Flask, request
 from google.cloud import pubsub_v1
 import os, json
 
+import logging
+logging.basicConfig(level=logging.INFO)
+
+
 app = Flask(__name__)
 
 PROJECT_ID = os.environ.get("PROJECT_ID", "lanch-pipeline-v3")
-TOPIC_ID = "meli-webhooks"
+TOPIC_ID = "mercadolibre-webhooks"
 publisher = pubsub_v1.PublisherClient()
 topic_path = publisher.topic_path(PROJECT_ID, TOPIC_ID)
 
 @app.route("/", methods=["POST"])
 def webhook():
     data = request.get_json()
+    
     if not data:
+        logging.warning("No JSON received")
         return {"error": "No JSON received"}, 400
+    
+    logging.info(f"Received webhook: {json.dumps(data)}")
 
     future = publisher.publish(topic_path, json.dumps(data).encode("utf-8"))
     future.result()
